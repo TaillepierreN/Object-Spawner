ff43523027de4f46f6d716d0f237e67db5b000d5
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-01-06 15:54:54 +0100

    Added limit to spawnable items
    Ajouter une limite au nombre d’objets
    ○ Le nombre maximum d’objets instanciable doit pouvoir être fixé dans l’éditeur
    ○ Par défaut la limite sera 10
    ○ Si l’utilisateur atteint la limite d’objet, il ne fait plus apparaître d’objet même s’il
    clique

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] float _distance;
     //[SerializeField] [Range(0,3)]int _objectForm;
     //[SerializeField]List<Mesh> _Meshes;
+    [SerializeField] float _distance;
     [Tooltip("0: cube form 1: Capsule 2: Cylinder 3: Sphere")]
     [SerializeField][Range(0,3)] int _typeObj;
     [SerializeField] List<GameObject> _object;
+    [SerializeField] int _itemCount;
 
 
     Camera _camera;
     Vector3 _spawn;
     Vector3 _mousePos;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,10 +33,14 @@ public class GameManager : MonoBehaviour
     }
     private void SpawnCube()
     {
+        if(_itemCount<= 10)
+        {
         _mousePos = Input.mousePosition;
         _mousePos.z = _distance;
         _spawn = _camera.ScreenToWorldPoint(_mousePos);
         //_object.GetComponent<MeshFilter>().mesh = _Meshes[_objectForm];
         Instantiate(_object[_typeObj],_spawn,Quaternion.identity);
+        _itemCount++;
+        }
     }
 }
