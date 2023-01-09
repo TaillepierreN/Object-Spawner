50d13d946e19f8004b59e9e871b1e19d68281982
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2023-01-09 12:01:48 +0100

    spawner script
    Créer une nouvelle classe Spawner
    ○ La classe doit instancier des gameObjects dans une zone donnée sur la
    durée
    ○ contrairement au script précédent, pas besoin de clic
    ○ à la place définir une vitesse d’apparition d’objets: nb d’objet/secondes
    (modifiable dans l’inspecteur)
    ○ Les objets apparaissent au hasard dans une zone sphérique, avec un rayon à
    définir via l’inspecteur
    ○ Les objets apparaissent avec une couleur aléatoire
    ○ Définissez un nombre max d’objet (modifiable dans l’inspecteur)
    ■ Pareil, si le Spawner fait apparaître plus d’objet que la limite, on
    réutilise l’objet le plus ancien à la place

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,19 +5,54 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] GameObject _object;
-    [SerializeField] GameObject _spawnAreaCenter;
-    [SerializeField] float _spawnRadius;
-    
+    [SerializeField] int _objectLimit;
+    [SerializeField] float _spawnRadius, _spawnRate;
+    public List<GameObject> spawnedObjects;
 
-    public Vector3 SpawnPoints{
-        get{
-            return Random.insideUnitSphere * _spawnRadius;
+    Vector3 _spawnPoints;
+    Color[] color = { Color.blue, Color.green, Color.yellow, Color.red, Color.white };
+    GameObject _spawnedObject;
+    int _numberOfObjects, _tableCount;
+
+
+    private void Start()
+    {
+
+        InvokeRepeating("SpawnInSphere", 1f, _spawnRate);
+
+
+    }
+
+    private void SpawnInSphere()
+    {
+        if (_numberOfObjects < _objectLimit)
+        {
+            SpawnObjects();
+            _numberOfObjects++;
         }
+        else
+        {
+            if (_tableCount < _objectLimit - 1)
+            {
+                MoveObject();
+                _tableCount++;
+            } else {
+                MoveObject();
+                _tableCount = 0;
+            }
+        }
+
+    }
+    private void SpawnObjects()
+    {
+        _spawnPoints = Random.insideUnitSphere * _spawnRadius;
+        _spawnedObject = Instantiate(_object, _spawnPoints, Quaternion.identity);
+        _spawnedObject.GetComponent<Renderer>().material.color = color[Random.Range(0, 4)];
+        spawnedObjects.Add(_spawnedObject);
+    }
+    private void MoveObject()
+    {
+        spawnedObjects[_tableCount].transform.position = Random.insideUnitSphere * _spawnRadius;
     }
 
-    private void Start() {
-        Debug.Log("hello");
-    }
-    //ressource
-    //https://catlikecoding.com/unity/tutorials/object-management/spawn-zones/
 }
